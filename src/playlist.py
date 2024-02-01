4a3e8c8af5654d4c23f7382e2760b2141e871577
elenaludina0573 <elenaludina0@gmail.com>
2024-02-01 21:09:24 +0300

    Addendum_2 new file homework_5

--- src/playlist.py
+++ src/playlist.py
@@ -1,7 +1,8 @@
 import os
+import json
 from dotenv import load_dotenv
 import isodate
-from datetime import timedelta
+import datetime
 from googleapiclient.discovery import build
 
 
@@ -13,40 +14,38 @@ class PlayList:
 
     def __init__(self, playlist_id):
         self._playlist_id = playlist_id
-        self.response = self.youtube.playlists().list(id=self._playlist_id,
-                                                      part='snippet',
-                                                      ).execute()
-        self.title = self.response['items'][0]['snippet']['title']
+        self.playlist_info = self.youtube.playlists().list(id=playlist_id, part='contentDetails, snippet', ).execute()
+        self.playlist_videos = self.youtube.playlistItems().list(playlistId=playlist_id,
+                                                                 part='contentDetails, snippet',
+                                                                 maxResults=50,
+                                                                 ).execute()
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        self.video_response = self.youtube.videos().list(part='contentDetails,statistics',
+                                                         id=','.join(self.video_ids)
+                                                         ).execute()
+        self.title = self.playlist_info['items'][0]['snippet']['title']
         self.url = f"https://www.youtube.com/playlist?list={self._playlist_id}"
 
-    def __str__(self):
-        return f"{self.title}"
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о видео."""
+        print(json.dumps(self.video_response, indent=2, ensure_ascii=False))
 
     @property
-    def total_duration(self) -> timedelta:
-        self._playlist_id = 'PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn'
-        playlist_videos = self.youtube.playlistItems().list(playlistId=self._playlist_id,
-                                                            part='contentDetails',
-                                                            maxResults=50,
-                                                            ).execute()
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        video_response = self.youtube.videos().list(part='contentDetails,statistics',
-                                                    id=','.join(video_ids)
-                                                    ).execute()
-        total_duration: timedelta = timedelta(hours=0, minutes=0, seconds=0)
-        for video in video_response['items']:
+    def total_duration(self):
+        total_duration = datetime.timedelta()
+        for video in self.video_response['items']:
             iso_8601_duration = video['contentDetails']['duration']
             duration = isodate.parse_duration(iso_8601_duration)
-            total_duration += duration
-            return total_duration
+            total_duration += datetime.timedelta(seconds=duration.total_seconds())
+        return total_duration
 
     def show_best_video(self):
         max_like_count = 0
-        max_video_id = 0
+        max_video_id = ''
         for video in self.video_response['items']:
             like_count: int = video['statistics']['likeCount']
             video_id = video['id']
             if int(like_count) > int(max_like_count):
                 max_like_count = like_count
                 max_video_id = video_id
-                return f"https://youtu.be/{max_video_id}"
+        return f"https://youtu.be/{max_video_id}"
